52777f14da8a5054b64cb62fe4ea2d962cd68cfb
Maria Sattar <maria.sattar@norgic.com>
2021-12-21 12:13:42 +0500

    Connectivity socket

--- lib/src/core/config/config.dart
+++ lib/src/core/config/config.dart
@@ -1,4 +1,5 @@
 const URL = "https://stenant.vdotok.dev/API/";
+//const URL = "https://tenant-api.vdotok.dev/API/";
 const version = "v0/";
-//const String project_id = "125YLEY1";
-const String project_id = "";
+const String project_id = "125YLEY1";
+//const String project_id = "170JRJUI";
