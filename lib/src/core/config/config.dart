992c68fa4194d74b9ad23556b1b3e997cb94a210
Maria Sattar <maria.sattar@norgic.com>
2022-01-03 12:58:49 +0500

    m2m latest code

--- lib/src/core/config/config.dart
+++ lib/src/core/config/config.dart
@@ -1,5 +1,5 @@
-const URL = "https://stenant.vdotok.dev/API/";
+const URL = "https://tenant-api.vdotok.dev/API/";
 //const URL = "https://tenant-api.vdotok.dev/API/";
 const version = "v0/";
-const String project_id = "125YLEY1";
+const String project_id = "170JRJUI";
 //const String project_id = "170JRJUI";
